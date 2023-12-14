6d89a428d1f8d5278582e2753ced6e7cf7012c10
Palani <palanimohan06@gmail.com>
2023-12-14 18:26:02 +0530

    created main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+//including libraries
+#include <stdio.h>
+
+/*
+Name			: Palani M
+Date			:
+Title			:
+Sample Input	:
+Sample output	:
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("Arbitary_precision_calculator\n");
+
+    return 0;
+}
+
