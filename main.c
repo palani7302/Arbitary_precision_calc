4e3b9eebb62627529f651e52d0c618da819a84ce
Palani <palanimohan06@gmail.com>
2023-12-14 22:17:35 +0530

    updated main.c

--- main.c
+++ main.c
@@ -7,12 +7,26 @@ Date			:
 Title			:
 Sample Input	:
 Sample output	:
-*/
+ */
 
 int main(int argc, char *argv[])
 {
-	printf("Arbitary_precision_calculator\n");
 
-    return 0;
+	if ( argc == 4 )
+	{
+		printf("Arbitary_precision_calculator\n");
+	}
+	else if ( argc < 4 )
+	{
+		printf("INFO : Less no. of arguments\n");
+		printf("USAGE : ./a.out 34 26 +\n");
+	}
+	else if ( argc > 4 )
+	{
+		printf("INFO : Too many arguments\n");
+		printf("USAGE : ./a.out 34 26 +\n");
+	}
+
+	return 0;
 }
 
